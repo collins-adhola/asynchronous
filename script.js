cf25f06b1dfa0c0360520725091f2956265fa2b9
Collino <coadhola@gmail.com>
2022-01-25 23:48:02 +0000

    fetch

--- script.js
+++ script.js
@@ -3,8 +3,35 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = " ") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+
 ///////////////////////////////////////////////////////////////
-const getCountryData = function(country) {
+/**
+  const getCountryData = function(country) {
 
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v2/name/${country}`);
@@ -37,6 +64,61 @@ const getCountryData = function(country) {
 };
 
 getCountryData('Portugal');
-getCountryData('germany');
-getCountryData('uganda');
+ */
+
+
+//  Old way with callback inside
+// 
+
+
+
+
+// const request = fetch(`https://restcountries.com/v2/name/portugal`);
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`).then (function(
+//     response) {
+//     console.log(response);
+//     return response.json();
+
+//   }).then (function (data){
+//     console.log(data);
+//     renderCountry(data[0])
+//   });
+// };
+
+//Same with arrow - look above for details
+
+
+
+const getCountryData = function (country){
+  // Country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+  .then(response => response.json())
+  .then(data => {
+    renderCountry(data[0]);
+    console.log(data[0])
+    const neighbour = data[0].borders[0];
+
+    if(!neighbour) return;
+
+    //country 2
+    return fetch(
+      `https://restcountries.com/v2/alpha/${neighbour}` // is what goes in return below
+    );
+})
+  .then(response => response.json())
+  .then(data => renderCountry(data, 'neighbour'))
+  .catch(err => {
+    console.error(`${err} 💥💥💥`);
+    renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+  })
+};
+
+
+btn.addEventListener('click', function(){
+  getCountryData("kenya");
+
+})
 
